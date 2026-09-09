8411190ccf580f9fe6f4ef7ee7856810f5487f88
tvk <dtvk922@gmail.com>
2026-09-09 07:36:59 +0530

    monoalphabetic_cipher
    Removed comments and unnecessary whitespace.

--- monoalphabetic_cipher.py
+++ monoalphabetic_cipher.py
@@ -1,25 +1,17 @@
-# Monoalphabetic Cipher
-
 plain = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 key = "QWERTYUIOPASDFGHJKLZXCVBNM"
-
 text = input("Enter text: ").upper()
-
 encrypted = ""
-
 for ch in text:
     if ch in plain:
         encrypted += key[plain.index(ch)]
     else:
         encrypted += ch
-
 decrypted = ""
-
 for ch in encrypted:
     if ch in key:
         decrypted += plain[key.index(ch)]
     else:
         decrypted += ch
-
 print("Encrypted:", encrypted)
 print("Decrypted:", decrypted)

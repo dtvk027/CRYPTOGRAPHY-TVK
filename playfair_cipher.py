7d86b1d533c552a8c1ae9b717fa62ff764114d2e
tvk <dtvk922@gmail.com>
2026-09-09 07:33:33 +0530

    Playfair cipher implementation

--- playfair_cipher.py
+++ playfair_cipher.py
@@ -1,65 +1,60 @@
-# Playfair Cipher
-# Fixed 5x5 matrix from the original program. J is treated as I.
-
-matrix = [
-    ['M', 'O', 'N', 'A', 'R'],
-    ['C', 'H', 'Y', 'B', 'D'],
-    ['E', 'F', 'G', 'I', 'K'],
-    ['L', 'P', 'Q', 'S', 'T'],
-    ['U', 'V', 'W', 'X', 'Z']
-]
-
-def position(ch):
-    if ch == 'J':
-        ch = 'I'
-
-    for i in range(5):
-        for j in range(5):
-            if matrix[i][j] == ch:
-                return i, j
-
-    return None
-
-s = input("Enter 2 letters: ").upper()
-
-if len(s) != 2 or not all('A' <= ch <= 'Z' for ch in s):
-    print("Error: Enter exactly 2 letters.")
-else:
-    p1 = position(s[0])
-    p2 = position(s[1])
-
-    if p1 is None or p2 is None:
-        print("Error: Invalid character.")
+key=input("Enter key: ").upper().replace("J","I")
+pt=input("Enter plaintext: ").upper().replace("J","I")
+seen=""
+for c in key+"ABCDEFGHIKLMNOPQRSTUVWXYZ":
+    if c not in seen and c.isalpha():
+        seen+=c
+m=[]
+for i in range(5):
+    m.append(list(seen[i*5:i*5+5]))
+p=""
+for c in pt:
+    if c.isalpha():
+        p+=c
+prep=""
+i=0
+while i<len(p):
+    x=p[i]
+    y=p[i+1] if i+1<len(p) else "X"
+    if x==y:
+        prep+=x+"X"
+        i+=1
     else:
-        r1, c1 = p1
-        r2, c2 = p2
-
-        # Encryption
-        if r1 == r2:
-            e1 = matrix[r1][(c1 + 1) % 5]
-            e2 = matrix[r2][(c2 + 1) % 5]
-        elif c1 == c2:
-            e1 = matrix[(r1 + 1) % 5][c1]
-            e2 = matrix[(r2 + 1) % 5][c2]
-        else:
-            e1 = matrix[r1][c2]
-            e2 = matrix[r2][c1]
-
-        encrypted = e1 + e2
-        print("Encrypted:", encrypted)
-
-        # Decryption
-        r1, c1 = position(e1)
-        r2, c2 = position(e2)
-
-        if r1 == r2:
-            d1 = matrix[r1][(c1 - 1) % 5]
-            d2 = matrix[r2][(c2 - 1) % 5]
-        elif c1 == c2:
-            d1 = matrix[(r1 - 1) % 5][c1]
-            d2 = matrix[(r2 - 1) % 5][c2]
-        else:
-            d1 = matrix[r1][c2]
-            d2 = matrix[r2][c1]
-
-        print("Decrypted:", d1 + d2)
+        prep+=x+y
+        i+=2
+if len(prep)%2!=0:
+    prep+="X"
+ct=""
+for i in range(0,len(prep),2):
+    x=prep[i]
+    y=prep[i+1]
+    for r in range(5):
+        for cc in range(5):
+            if m[r][cc]==x:
+                r1,c1=r,cc
+            if m[r][cc]==y:
+                r2,c2=r,cc
+    if r1==r2:
+        ct+=m[r1][(c1+1)%5]+m[r2][(c2+1)%5]
+    elif c1==c2:
+        ct+=m[(r1+1)%5][c1]+m[(r2+1)%5][c2]
+    else:
+        ct+=m[r1][c2]+m[r2][c1]
+print("Ciphertext:",ct)
+dt=""
+for i in range(0,len(ct),2):
+    x=ct[i]
+    y=ct[i+1]
+    for r in range(5):
+        for cc in range(5):
+            if m[r][cc]==x:
+                r1,c1=r,cc
+            if m[r][cc]==y:
+                r2,c2=r,cc
+    if r1==r2:
+        dt+=m[r1][(c1-1)%5]+m[r2][(c2-1)%5]
+    elif c1==c2:
+        dt+=m[(r1-1)%5][c1]+m[(r2-1)%5][c2]
+    else:
+        dt+=m[r1][c2]+m[r2][c1]
+print("Decrypted:",dt)
